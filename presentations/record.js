0bb1cdaffc3eb08f0552e82ebbc69a0901986475
Akshat Gupta <akshatvg23@gmail.com>
2020-09-13 18:04:14 +0530

    changes

--- presentations/record.js
+++ presentations/record.js
@@ -28,9 +28,6 @@ function stopRecordAudio() {
 
         var transcript = event.results[current][0].transcript;
 
-        // Content = Content + transcript + "<br>";
-        // document.getElementById("chatbox").innerHTML = "<p class='chats'>" + Content + "</p><br>";
-
         Content = Content + transcript;
         Content = Content;
 
@@ -73,20 +70,37 @@ function stopRecordAudio() {
                 if (this.status == 200) {
                     var response = JSON.parse(this.responseText);
                     console.log(response.type);
-                    if (response.type == "bullet") {
-                        document.getElementById("para1").innerHTML = "<p class='white-text'>" + response.bullet + "</p>";
-                    }
+                    document.getElementById("para1").innerHTML = "<p class='white-text'>" + response.bullet + "</p>";
                     if (response.type == "image") {
-                        $("#image1").append(`<img src="" alt="" class="responsive-img"/>`);
+                        imageOf = response.image;
+                        var xhr2 = new XMLHttpRequest();
+                        xhr2.open(
+                            "GET",
+                            "https://api.pexels.com/v1/search?query=" + imageOf,
+                            true
+                        );
+                        xhr2.setRequestHeader("Content-Type", "application/json");
+                        xhr2.setRequestHeader("Authorization", "563492ad6f917000010000012da74593e6f44f69a1a08440e8772609");
+                        xhr2.send(JSON.stringify(text));
+                        xhr2.onload = function () {
+                            if (this.status == 200) {
+                                var response = JSON.parse(this.responseText);
+                                console.log(response);
+                                document.getElementById("title").innerHTML = response.title;
+                                var imageURL = this.responseText.url;
+                                $("#image1").append(`<img src="${imageURL}" alt="" class="responsive-img"/>`);
+                            }
+                        };
                     }
                     if (response.type == "plot") {
-                        if (response.type.plot == "bar") {
+                        console.log(response.plot);
+                        if (response.plot == "bar") {
                             $("#graph1").append(`<img src="../assets/img/barChart.png" alt="" class="responsive-img"/>`);
                         }
-                        if (response.type.plot == "line") {
+                        if (response.plot == "line") {
                             $("#graph1").append(`<img src="../assets/img/lineChart.png" alt="" class="responsive-img"/>`);
                         }
-                        if (response.type.plot == "pie") {
+                        if (response.plot == "pie") {
                             $("#graph1").append(`<img src="../assets/img/pieChart.png" alt="" class="responsive-img"/>`);
                         }
                     }
@@ -114,17 +128,36 @@ function stopRecordAudio() {
                         document.getElementById("para2").innerHTML = "<p class='white-text'>" + response.bullet + "</p>";
                     }
                     if (response.type == "image") {
-                        $("#image1").append(`<img src="" alt="" class="responsive-img"/>`);
+                        imageOf = response.image;
+                        var xhr2 = new XMLHttpRequest();
+                        xhr2.open(
+                            "GET",
+                            "https://api.pexels.com/v1/search?query=" + imageOf,
+                            true
+                        );
+                        xhr2.setRequestHeader("Content-Type", "application/json");
+                        xhr2.setRequestHeader("Authorization", "563492ad6f917000010000012da74593e6f44f69a1a08440e8772609");
+                        xhr2.send(JSON.stringify(text));
+                        xhr2.onload = function () {
+                            if (this.status == 200) {
+                                var response = JSON.parse(this.responseText);
+                                console.log(response);
+                                document.getElementById("title").innerHTML = response.title;
+                                var imageURL = this.responseText.url;
+                                $("#image2").append(`<img src="${imageURL}" alt="" class="responsive-img"/>`);
+                            }
+                        };
                     }
                     if (response.type == "plot") {
-                        if (response.type.plot == "bar") {
-                            $("#graph1").append(`<img src="../assets/img/barChart.png" alt="" class="responsive-img"/>`);
+                        console.log(response.plot);
+                        if (response.plot == "bar") {
+                            $("#graph2").append(`<img src="../assets/img/barChart.png" alt="" class="responsive-img"/>`);
                         }
-                        if (response.type.plot == "line") {
-                            $("#graph1").append(`<img src="../assets/img/lineChart.png" alt="" class="responsive-img"/>`);
+                        if (response.plot == "line") {
+                            $("#graph2").append(`<img src="../assets/img/lineChart.png" alt="" class="responsive-img"/>`);
                         }
                         if (response.type.plot == "pie") {
-                            $("#graph1").append(`<img src="../assets/img/pieChart.png" alt="" class="responsive-img"/>`);
+                            $("#graph2").append(`<img src="../assets/img/pieChart.png" alt="" class="responsive-img"/>`);
                         }
                     }
                 }
